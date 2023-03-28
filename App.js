1b59527ba5f67099064536f52a2d7ba1df7ac81a
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-03-28 14:28:56 -0400

    basic folders created

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import {Stack, useRouter} from 'expo-router'
+
 
 export default function App() {
   return (
